366618ab6b35477e2eeac7e1606f1449ca30dba4
Vinay Joshi <vinayjoshi450@gmail.com>
2023-06-02 18:29:49 -0400

    Created individual screens for each game and linked selection buttons to each screen

--- src/main/java/com/example/project/SelectController.java
+++ src/main/java/com/example/project/SelectController.java
@@ -1,27 +1,45 @@
 package com.example.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class SelectController {
-    @FXML
-    private Button game1Button;
-    @FXML
-    private Button game2Button;
-    @FXML
-    private Button game3Button;
-
-    @FXML
-    private void initialize(){
-        game1Button.setOnAction(event -> {
-            // Code to start Flow
-        });
-        game2Button.setOnAction(event -> {
-            // Code to start 2048
-        });
-        game3Button.setOnAction(event -> {
-            // Code to start Codenames
-        });
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void flowSwitch(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FlowScreen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+    public void twentySwitch(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TwentyScreen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void codeNameSwitch(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("CodeNamesScreen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
 }
