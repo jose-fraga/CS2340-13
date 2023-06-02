4017aa26cb9dbf0b3641b51bcf7231addd898259
Vinay Joshi <vinayjoshi450@gmail.com>
2023-06-02 15:31:25 -0400

    Created base home screen with basic tools set up.

--- src/main/java/com/example/project/SelectController.java
+++ src/main/java/com/example/project/SelectController.java
@@ -1,5 +1,27 @@
 package com.example.project;
 
-public class SelectController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SpinnerValueFactory;
 
+public class SelectController {
+    @FXML
+    private Button game1Button;
+    @FXML
+    private Button game2Button;
+    @FXML
+    private Button game3Button;
+
+    @FXML
+    private void initialize(){
+        game1Button.setOnAction(event -> {
+            // Code to start Flow
+        });
+        game2Button.setOnAction(event -> {
+            // Code to start 2048
+        });
+        game3Button.setOnAction(event -> {
+            // Code to start Codenames
+        });
+    }
 }
