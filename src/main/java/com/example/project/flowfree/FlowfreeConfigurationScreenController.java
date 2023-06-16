12ccdc686e4a0433a33332f4cdf6d93ee6fc4c7f
jose-fraga <josemendezfraga@gmail.com>
2023-06-16 15:51:36 -0400

    deleted config on flow free controller

--- src/main/java/com/example/project/flowfree/FlowfreeConfigurationScreenController.java
+++ src/main/java/com/example/project/flowfree/FlowfreeConfigurationScreenController.java
@@ -6,12 +6,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FlowfreeConfigurationScreenController implements Initializable {
-    private FlowfreeConfigSingleton config;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         System.out.println("Flow Free Configuration Screen");
-        config = FlowfreeConfigSingleton.getInstance();
     }
 
 }
