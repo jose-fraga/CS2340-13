1fd79d42f9b3931578f1676a1f38ec03ade41bfb
SArunD <daverpallysaiarun@gmail.com>
2023-06-17 12:21:30 -0400

    MMF#1 Working Progess #3

---
+++ src/main/java/com/example/project/flowfree/FFPane.java
@@ -0,0 +1,17 @@
+package com.example.project.flowfree;
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+public class FFPane extends Pane {
+    private GridItem gridItem;
+
+    public FFPane(GridItem gridItem) {
+        this.gridItem = gridItem;
+    }
+
+    public FFPane(GridItem gridItem, Node content) {
+        super(content);
+        this.gridItem = gridItem;
+    }
+}
