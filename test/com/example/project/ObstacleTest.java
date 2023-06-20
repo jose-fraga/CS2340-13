1c359b3978ff3786a0e843be9f045034155365a2
Vinay Joshi <vinayjoshi450@gmail.com>
2023-06-19 21:22:22 -0400

    JUnit Test Obstacle Tests

--- test/com/example/project/ObstacleTest.java
+++ test/com/example/project/ObstacleTest.java
@@ -1,2 +1,21 @@
-package com.example.project;public class ObstacleTest {
+package com.example.project;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.project.flowfree.Obstacle;
+import org.junit.jupiter.api.Test;
+
+public class ObstacleTest {
+    @Test
+    void testDestroyObstacle() {
+        int health = 15;
+        Obstacle o1 = new Obstacle(health);
+        o1.destroy();
+        assertTrue(o1.getHitPoints() == health, "Destroy is not working");
+    }
+    @Test
+    void testObstacleCreated() {
+        Obstacle o1 = new Obstacle(15);
+        assertNull(o1, "Obstacle is null");
+    }
 }
