f5bda04129ae8e4d91183253781bc9f6f89e2ad0
Vinay Joshi <vinayjoshi450@gmail.com>
2023-06-26 22:06:38 -0400

    Resize Gridpane, Check answer logic, key event on submit
    Resize gridpane to correct word length and attempts. Added check answer method and key events for when the user submits the entry. Temporary change color method that alters the state of each cells color

--- src/main/java/com/example/project/wordle/WRLGameController.java
+++ src/main/java/com/example/project/wordle/WRLGameController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -25,8 +26,8 @@ public class WRLGameController implements Initializable {
     }
 
     public void populate() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
                 StackPane currPane = new StackPane();
                 currPane.getChildren().add(new Label());
                 currPane.setAlignment(Pos.CENTER);
@@ -50,6 +51,57 @@ public class WRLGameController implements Initializable {
 //            ((Label) gridPane.getChildren().get(--index)).setText("");
             StackPane curr = (StackPane) gridPane.getChildren().get(--index);
             ((Label) curr.getChildren().get(0)).setText("");
+        } else if (e.getCode() == KeyCode.ENTER) {
+            char[] userAnswer = new char[5];
+            for(int i=0; i < 5;i++) {
+                StackPane curr = (StackPane) gridPane.getChildren().get(index);
+                ((Label) curr.getChildren().get(0)).getText();
+               String temp = getGridContents(currentRow, i).toString();
+                userAnswer[i] = temp.charAt(0);
+            }
+            checkAnswer(userAnswer);
+        }
+    }
+    //temp testing variable
+    private String correctAnswer = "apple";
+    public int currentRow = 0;
+    public void checkAnswer(char[] userAnswer) {
+        String answer = new String(userAnswer);
+//        if(DictionaryService.checkIfWordExists(answer)){
+//            for(int column = 0; column < answer.length(); column++) {
+//                String letter = correctAnswer.substring(column, column+1);
+//                if(letter.equals(correctAnswer.substring(column,column+1))){
+//                    changeColor(green, currentRow, column);
+//                } else if(correctAnswer.contains(letter)) {
+//                     changeColor(yellow, currentRow, column);
+//                } else {
+//                    changeColor(white, currentRow, column);
+//                }
+//            }
+//            currentRow++;
+//        } else {
+//            System.out.println("Word is not valid");
+        }
+
+    public Node getGridContents(Integer row, Integer column){
+        for (Node node : gridPane.getChildren()) {
+            if(GridPane.getColumnIndex(node) == row && GridPane.getColumnIndex(node) == column){
+                return node;
+            }
+        }
+        return null;
+    }
+    public void changeColor(String color, Integer row, Integer Column) {
+        switch(color) {
+            case "green":
+                //set row and column to green
+                break;
+            case "yellow":
+                //set row and column to green
+                break;
+            case "white":
+                //set row and column to green
+                break;
         }
     }
 }
