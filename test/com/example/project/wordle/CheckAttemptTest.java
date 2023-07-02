e9f852d8edd518f134754d07f71a71b4955be05a
Vinay Joshi <vinayjoshi450@gmail.com>
2023-07-02 12:32:03 -0400

    Tests values needed to correctly pass in checkAttempt()

---
+++ test/com/example/project/wordle/CheckAttemptTest.java
@@ -0,0 +1,20 @@
+package com.example.project.wordle;
+
+import com.example.project.wordle.enums.Status;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CheckAttemptTest {
+    @Test
+    void testAttemptedLetterConstructor() {
+        int index = 1;
+        AttemptedLetter al = new AttemptedLetter(index);
+        assertNotNull(al);
+    }
+    @Test
+    void testDefaultStatus() {
+        AttemptedLetter al = new AttemptedLetter(1);
+        assertEquals(al.getStatus(),Status.EMPTY);
+    }
+}
