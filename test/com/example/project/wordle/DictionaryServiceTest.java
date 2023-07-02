b47802a40ad0732730f98d65b8768032b4cac1b2
SArunD <daverpallysaiarun@gmail.com>
2023-07-02 04:08:28 -0400

    Added two test cases for Dictionary.java

---
+++ test/com/example/project/wordle/DictionaryServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.project.wordle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryServiceTest {
+    @Test
+    void checkValidityEnv() {
+        assertNotNull(System.getenv("NINJA_API_URL"));
+        assertNotNull(System.getenv("NINJA_API_KEY"));
+    }
+
+    @Test
+    void checkValidityOfWords() {
+        String[] words = {"apple", "ap", "abcde", "mapple", "amazing"};
+        assertTrue(DictionaryService.checkValidity(words[0]));
+        assertFalse(DictionaryService.checkValidity(words[1]));
+        assertFalse(DictionaryService.checkValidity(words[2]));
+        assertFalse(DictionaryService.checkValidity(words[3]));
+        assertTrue(DictionaryService.checkValidity(words[4]));
+    }
+}
