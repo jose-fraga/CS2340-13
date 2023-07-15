b2771dfd1d3c0f23d73b22cd6e12041cee330d96
SArunD <daverpallysaiarun@gmail.com>
2023-07-15 12:56:21 -0400

    Plug into class observables and update screens for different players

--- src/main/java/com/example/project/codenames/controllers/CNGameScreenController.java
+++ src/main/java/com/example/project/codenames/controllers/CNGameScreenController.java
@@ -1,20 +1,29 @@
 package com.example.project.codenames.controllers;
 
-import com.example.project.codenames.CNGame;
-import com.example.project.codenames.Round;
-import com.example.project.codenames.WordPane;
+import com.example.project.codenames.*;
+import com.example.project.codenames.enums.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CNGameScreenController implements Initializable {
+public class CNGameScreenController implements Initializable, PropertyChangeListener {
+    @FXML private BorderPane borderPane;
     @FXML private GridPane gridPane;
+    @FXML private HBox spyMasterBox;
+    @FXML private TextField clueInput;
+
+    private Round round;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -25,15 +34,19 @@ public class CNGameScreenController implements Initializable {
 
     public void populate() {
         CNGame.getGameInstance().createRound();
-        Round round = CNGame.getGameInstance().getRound();
+        CNGame.getGameInstance().getRound().addPropertyChangeListener(this);
+        this.round = CNGame.getGameInstance().getRound();
         int count = 0;
         for (int i = 0; i < gridPane.getRowCount(); i++) {
             for (int j = 0; j < gridPane.getColumnCount(); j++) {
-                gridPane.add(new WordPane(round.getWords().get(count)), j, i);
+                if (round.getActiveTeam().getCurrentPlayer() == Player.SPY_MASTER) {
+                    gridPane.add(new WordPane(round.getWords().get(count),false), j, i);
+                } else {
+                    gridPane.add(new WordPane(round.getWords().get(count),true), j, i);
+                }
                 count++;
             }
         }
-
     }
 
     public void handle() {
@@ -41,11 +54,34 @@ public class CNGameScreenController implements Initializable {
             if (!(item instanceof Group)) {
                 WordPane curr = (WordPane) item;
                 VBox currBox = (VBox) curr.getChildren().get(0);
-                currBox.getChildren().get(1).addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+
+                if (this.round.getActiveTeam().getCurrentPlayer() == Player.OPERATIVE) {
+                    currBox.getChildren().get(1).addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
 //                    curr.getWord().changeSelected();
+                        curr.getWord().select();
+                        if (curr.getWord().getIsSelected()) {
+                            curr.addBackground();
+                        }
+                    });
+                } else {
                     curr.addBackground();
-                });
+                }
             }
         });
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println(evt);
+        if (evt.getPropertyName().equals("activeTeam")) { //TODO make enums for propertynames
+            Team activeTeam = (Team) evt.getNewValue();
+            // switch screen to buffer screen so spy master can give clue
+        }
+    }
+
+    @FXML public void switchToOperative() {
+        System.out.println(clueInput.getText());
+        this.round.getActiveTeam().setCurrentPlayer(Player.OPERATIVE);
+        spyMasterBox.setVisible(false);
     }
 }
