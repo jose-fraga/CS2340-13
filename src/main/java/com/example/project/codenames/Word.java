52fcf7703402283017bfd5605598db1595de60f4
Amir D. Sattari <1408170+ggamir@users.noreply.github.com>
2023-07-15 12:55:41 -0400

    Add observerer pattern throughout Word, Round, and Team classes

--- src/main/java/com/example/project/codenames/Word.java
+++ src/main/java/com/example/project/codenames/Word.java
@@ -2,14 +2,19 @@ package com.example.project.codenames;
 
 import com.example.project.codenames.enums.Type;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 // TODO: make words observable so that we can take actions when it is selected.
 public class Word {
     private final String word;
     private final String definition;
     private Type type;
     private boolean isSelected;
+    private PropertyChangeSupport support;
 
     public Word(Type type, String word, String definition) {
+        this.support = new PropertyChangeSupport(this);
         this.word = word;
         this.definition = definition;
         this.type = type;
@@ -20,8 +25,21 @@ public class Word {
 
     public boolean getIsSelected() { return this.isSelected; }
 
+    public void select() {
+        this.isSelected = true;
+        this.support.firePropertyChange("isSelected", false, true);
+    }
+
     public Type getType() { return this.type; }
     public void setType(Type type) { this.type = type; }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.support.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.support.removePropertyChangeListener(listener);
+    }
 
     @Override
     public String toString() {
